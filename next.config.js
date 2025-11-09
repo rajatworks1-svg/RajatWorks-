df9bb20f658bc722b63fa5603ec76b3b3061eb42
Rajat Kumar Dua <rajatworks1@gmail.com>
2025-11-09 21:51:42 +0530

    FINAL FIX: Disabled metadata image loader in next.config.js to bypass corrupt icon file error.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // This setting is CRITICAL for Vercel to recognize the 'app' directory
     experimental: {
         appDir: true,
+        // FIX: Disable the metadata image loader that is failing the build.
+        // We will manage favicon/icon manually or delete the corrupt file.
+        nextMetadataImageLoader: false,
     },
 };
 
